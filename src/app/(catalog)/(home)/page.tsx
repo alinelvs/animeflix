32af6c472c8da91b2f1de418b91a30781e64613a
alinelvs <aline.semenov@gmail.com>
2024-03-10 16:40:50 -0300

    fix: adjusted prettier plugin configuration for tailwind css

--- src/app/(catalog)/(home)/page.tsx
+++ src/app/(catalog)/(home)/page.tsx
@@ -5,55 +5,55 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className="grid grid-cols-3 gap-6">
-      <Link href="/" className="relative rounded-lg overflow-hidden group">
+      <Link href="/" className="group relative overflow-hidden rounded-lg">
         <Image
           src="/dragon-ball.png"
-          className="group-hover:scale-105 transition-transform duration-500"
+          className="transition-transform duration-500 group-hover:scale-105"
           width={920}
           height={920}
           quality={100}
           alt=""
         />
-        <div className="absolute bottom-4 left-4 right-4 bg-black bg-opacity-50 text-white text-center p-2">
-          <span className="text-sm truncate">Dragon Ball</span>
-          <span className="flex items-center justify-center bg-pink-500 rounded-full p-1 mt-2">
-            <PlayCircle className="w-6 h-6" />
+        <div className="absolute bottom-4 left-4 right-4 bg-black bg-opacity-50 p-2 text-center text-white">
+          <span className="truncate text-sm">Dragon Ball</span>
+          <span className="mt-2 flex items-center justify-center rounded-full bg-pink-500 p-1">
+            <PlayCircle className="h-6 w-6" />
             <span className="ml-2 font-semibold">Assistir</span>
           </span>
         </div>
       </Link>
 
-      <Link href="/" className="relative rounded-lg overflow-hidden group">
+      <Link href="/" className="group relative overflow-hidden rounded-lg">
         <Image
           src="/one-piece.png"
-          className="group-hover:scale-105 transition-transform duration-500"
+          className="transition-transform duration-500 group-hover:scale-105"
           width={920}
           height={920}
           quality={100}
           alt=""
         />
-        <div className="absolute bottom-4 left-4 right-4 bg-black bg-opacity-50 text-white text-center p-2">
-          <span className="text-sm truncate">One Piece</span>
-          <span className="flex items-center justify-center bg-pink-500 rounded-full p-1 mt-2">
-            <PlayCircle className="w-6 h-6" />
+        <div className="absolute bottom-4 left-4 right-4 bg-black bg-opacity-50 p-2 text-center text-white">
+          <span className="truncate text-sm">One Piece</span>
+          <span className="mt-2 flex items-center justify-center rounded-full bg-pink-500 p-1">
+            <PlayCircle className="h-6 w-6" />
             <span className="ml-2 font-semibold">Assistir</span>
           </span>
         </div>
       </Link>
 
-      <Link href="/" className="relative rounded-lg overflow-hidden group">
+      <Link href="/" className="group relative overflow-hidden rounded-lg">
         <Image
           src="/naruto.png"
-          className="group-hover:scale-105 transition-transform duration-500"
+          className="transition-transform duration-500 group-hover:scale-105"
           width={920}
           height={920}
           quality={100}
           alt=""
         />
-        <div className="absolute bottom-4 left-4 right-4 bg-black bg-opacity-50 text-white text-center p-2">
-          <span className="text-sm truncate">Naruto</span>
-          <span className="flex items-center justify-center bg-pink-500 rounded-full p-1 mt-2">
-            <PlayCircle className="w-6 h-6" />
+        <div className="absolute bottom-4 left-4 right-4 bg-black bg-opacity-50 p-2 text-center text-white">
+          <span className="truncate text-sm">Naruto</span>
+          <span className="mt-2 flex items-center justify-center rounded-full bg-pink-500 p-1">
+            <PlayCircle className="h-6 w-6" />
             <span className="ml-2 font-semibold">Assistir</span>
           </span>
         </div>
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -11,7 +11,7 @@ export function Header() {
         </Link>
 
         <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
-          <Search className="w-5 h-5" />
+          <Search className="h-5 w-5" />
 
           <input
             placeholder="Buscar animes..."
@@ -26,7 +26,7 @@ export function Header() {
           <span className="text-sm">Salvos</span>
         </div>
 
-        <div className="w-px h-4 bg-zinc-700" />
+        <div className="h-4 w-px bg-zinc-700" />
 
         <Link href="" className="flex items-center gap-2 hover:underline">
           <span className="text-sm">Perfil</span>
