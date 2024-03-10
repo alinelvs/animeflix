77bbfa8e974c5309eec5264b8a67af793588a8b1
alinelvs <aline.semenov@gmail.com>
2024-03-10 20:58:30 -0300

    feat(home): created loading on home

--- src/app/(catalog)/(home)/loading.tsx
+++ src/app/(catalog)/(home)/loading.tsx
@@ -1,3 +1,14 @@
+import { Skeleton } from '@/components/skeleton'
+
 export default function HomeLoading() {
-  return <p>Carregando ...</p>
+  return (
+    <div className="grid h-full grid-cols-3 gap-6">
+      <Skeleton className="h-[350px]" />
+      <Skeleton className="" />
+      <Skeleton className="" />
+      <Skeleton className="h-[350px]" />
+      <Skeleton className="" />
+      <Skeleton className="" />
+    </div>
+  )
 }
