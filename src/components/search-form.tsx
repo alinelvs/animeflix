b0d4c72a4fcc7005a681a5312a2853e81cbda884
alinelvs <aline.semenov@gmail.com>
2024-03-13 13:56:31 -0300

    feat(search-form): added clear input on search and search on icon click

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -2,27 +2,35 @@
 
 import { Search } from 'lucide-react'
 import { useRouter, useSearchParams } from 'next/navigation'
-import { FormEvent } from 'react'
+import { FormEvent, useState } from 'react'
 
 export function SearchForm() {
   const router = useRouter()
   const searchParams = useSearchParams()
 
-  const query = searchParams.get('q')
+  const [searchQuery, setSearchQuery] = useState<string>(() => {
+    const query = searchParams.get('q')
+    return query ?? ''
+  })
 
   function handleSearch(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
 
-    const formData = new FormData(event.currentTarget)
-    const data = Object.fromEntries(formData)
-
-    const query = data.q
-
-    if (!query) {
+    if (!searchQuery) {
       return null
     }
 
-    router.push(`/search?q=${query}`)
+    router.push(`/search?q=${searchQuery}`)
+    setSearchQuery('')
+  }
+
+  function handleSearchIconClick() {
+    if (!searchQuery) {
+      return null
+    }
+
+    router.push(`/search?q=${searchQuery}`)
+    setSearchQuery('')
   }
 
   return (
@@ -30,11 +38,15 @@ export function SearchForm() {
       onSubmit={handleSearch}
       className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700"
     >
-      <Search className="h-5 w-5 text-zinc-500" />
+      <Search
+        className="h-5 w-5 cursor-pointer text-zinc-500"
+        onClick={handleSearchIconClick}
+      />
 
       <input
         name="q"
-        defaultValue={query ?? ''}
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
         placeholder="Search animes..."
         className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
         required
